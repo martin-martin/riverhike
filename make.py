9f9938f8a327fd0741b59799569a79e4c21e9c7c
martin-martin <breuss.martin@gmail.com>
2020-01-13 22:51:12 +0100

    Replace indentations with spaces

--- make.py
+++ make.py
@@ -1,27 +1,26 @@
 import json
 from jinja2 import Template
 
-
 with open('data.json') as fin:
     data = json.load(fin)
 
 raw = Template("""<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<!-- Font from here: http://www.fontspace.com/jackster-productions/pokemon-gb -->
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-100873215-1', 'auto');
-	  ga('send', 'pageview');
-	</script>
-	<title>{{ page_title }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Font from here: http://www.fontspace.com/jackster-productions/pokemon-gb -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-100873215-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <title>{{ page_title }}</title>
 </head>
 <body>
 <div class="container-fluid">
@@ -30,10 +29,10 @@ raw = Template("""<!DOCTYPE html>
         <p class="display-5">{{ subtitle }}</p>
     </div>
     <div class="container">
-		<img class="mx-auto d-block mb-4" src="img/gif/{{ map }}" alt="Pokémon style map of the Bay Area">
-		<div class="row">
-		{% for n in data['header']['notes'] %}
-		{% if loop.index == 4 %}
+        <img class="mx-auto d-block mb-4" src="img/gif/{{ map }}" alt="Pokémon style map of the Bay Area">
+        <div class="row">
+        {% for n in data['header']['notes'] %}
+        {% if loop.index == 4 %}
         <div class="col-12 mb-4">
             <div class="card">
                 <div class="card-body">{{ n['text'] }}</div>
@@ -46,10 +45,10 @@ raw = Template("""<!DOCTYPE html>
             </div>
         </div>
         {% endif %}
-		{% endfor %}
-		</div>
-	</div>
-	{% for s in data['body'] %}
+        {% endfor %}
+        </div>
+    </div>
+    {% for s in data['body'] %}
     <section>
         <div class="jumbotron day-{{ loop.index }}">
           <h2 class="display-5">{{ s['hero']['title'] }}</h2>
@@ -106,8 +105,8 @@ raw = Template("""<!DOCTYPE html>
             </div>
         </section>
     </section>
-	{% endfor %}
-	</div>
+    {% endfor %}
+    </div>
 </body>
 </html>""")
 
@@ -118,32 +117,31 @@ html = raw.render(page_title='pokéblog',
                   data=data
                   )
 
-
 css = """@font-face {
-	font-family: "Pokemon";
-	src: url("font/pokemon_gb.ttf");
+    font-family: "Pokemon";
+    src: url("font/pokemon_gb.ttf");
 }
 
 body {
-	font-family: Pokemon, Verdana, Tahoma, monospace;
+    font-family: Pokemon, Verdana, Tahoma, monospace;
 }
 
 .card {  /* override Bootstrap card border for more original GB Pkmn style */
-	border: 6px double black;
+    border: 6px double black;
 }
 
 table img { width: 60px; }
 
 ul {
-	list-style-image: url("img/pokeball-bullet-point.png");
+    list-style-image: url("img/pokeball-bullet-point.png");
 }
 
 .jumbotron {  /* general code to allow for full-width background images with text on top (hero) */
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	text-align: center;
-	color: white;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+    color: white;
 }
 
 /* individual section background images below */"""
